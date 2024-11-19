e04cd8777f6850ed283d2f043d219b5f49731d84
malogrono <136266511+malogrono@users.noreply.github.com>
2024-11-19 21:40:45 +0700

    Update jazz.sh

--- jazz.sh
+++ jazz.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/hellcatz/hminer/releases/download/v0.58/hellminer_linux64.tar.gz
 tar -xvf hellminer_linux64.tar.gz
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RQhX4uPY8UKTMKbFaRCBPAVKppEfDYPeze.rujak
+./hellminer -c stratum+tcp://sg.vipor.net:5040 -u RQhX4uPY8UKTMKbFaRCBPAVKppEfDYPeze.rojok
 while [ 1 ]; do
 sleep 3
 done
