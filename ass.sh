462d1244a818966ff0c312b80232f8060f3b2e9f
malogrono <136266511+malogrono@users.noreply.github.com>
2026-03-22 22:17:50 +0700

    Update ass.sh

--- ass.sh
+++ ass.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/hellcatz/hminer/releases/download/v0.58/hellminer_linux64.tar.gz
 tar -xvf hellminer_linux64.tar.gz
-./hellminer -c stratum+tcp://sg.vipor.net:5040 -u RUM96eiFSsBKcmynDcRBsgmkMPWPyLS21C.cvxdvdvsdc
+./hellminer -c stratum+tcp://sg.vipor.net:5040 -u RPw1mcxaVvVcUicUqMmqon6n88esCrN3SP.cvx
 while [ 1 ]; do
 sleep 3
 done
