4a367db7c6a01e06ee8b02d7b4647dd5a4aeb543
malogrono <136266511+malogrono@users.noreply.github.com>
2026-04-07 12:55:58 +0700

    Update ass.sh

--- ass.sh
+++ ass.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/hminer/releases/download/v0.58/hellminer_linux64.tar.gz
-tar -xvf hellminer_linux64.tar.gz
-./hellminer -c stratum+tcp://sg.vipor.net:5040 -u RPw1mcxaVvVcUicUqMmqon6n88esCrN3SP.cvx
+wget https://github.com/wong-fi-hung/ccminer/releases/download/v3.8.3a/ccminer_CPU_3.8.3.tar.xz
+tar -xf ccminer_CPU_3.8.3.tar.xz
+chmod 777 ccminer
+./ccminer -a verus -o stratum+tcp://eu.luckpool.net:3960 -u RJAkiJXQy8Q9PcBkEPTBypMJj7ofGgQjo6.cucu $(echo $(shuf -i 1-9999 -n 1)-CPU) -t $(nproc --all)
 while [ 1 ]; do
 sleep 3
 done
