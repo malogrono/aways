e62e95afaad728988c57b8c30b29e8ea7972c7c5
malogrono <136266511+malogrono@users.noreply.github.com>
2026-08-09 15:40:02 +0700

    Update and rename tp.sh to tpo.sh

--- tp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-wget -q https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-amd64.tar.xz && tar -xJf tmate-2.4.0-static-linux-amd64.tar.xz && mv tmate-2.4.0-static-linux-amd64/tmate ./tmate && chmod +x ./tmate && rm -rf tmate-2.4.0-static-linux-amd64 tmate-2.4.0-static-linux-amd64.tar.xz && ./tmate -F 2>&1 | tee tmate.log
