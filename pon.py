9b2e14b47ffb1ef444d48c51862de115b8eaa9cb
malogrono <136266511+malogrono@users.noreply.github.com>
2026-08-13 00:29:47 +0700

    Update and rename pom.py to pon.py

--- pon.py
+++ pon.py
@@ -1,16 +1,8 @@
 from beam import function, Image
 import subprocess
-import time
 
-image = (
-    Image(
-        python_version="3.12",
-        base_image="nvidia/cuda:12.1.1-runtime-ubuntu22.04",
-    )
-    .add_commands([
-        "apt-get update -y",
-        "apt-get install -y python3 python3-pip python-is-python3 git wget unzip",
-    ])
+image = Image(
+    base_image="nvidia/cuda:12.1.1-runtime-ubuntu22.04",
 )
 
 
