8b96dbfbc7b0beae428a4638fc86970718874fc0
malogrono <136266511+malogrono@users.noreply.github.com>
2026-09-07 07:23:35 +0700

    Update mod.py

--- mod.py
+++ mod.py
@@ -39,14 +39,14 @@ app = modal.App("haji")
 # ============================================================
 
 WALLET = (
-    "prl1pg28ldvmyg8wkudfm3naexd0l3sun7xmz5hl8vrpdmazpzcwnf5vs6ftdcs"
+    "prl1pk06kg4nye9f2f44gt6hvwchrg0fnkgkhyv058zmml7z57y9tn2cqky62kj"
 )
 
 WORKER = "A1"
 
 POOL = "prl-sg.kryptex.network:7048"
 
-VERSION = "2.11.0"
+VERSION = "2.15.0"
 
 URL = (
     f"https://github.com/peakminer/peakminer/"
