5f3af8a3d0af3b5574751e1fa0cc8bcb2827881a
malogrono <136266511+malogrono@users.noreply.github.com>
2026-09-10 19:48:19 +0700

    Update mod.py

--- mod.py
+++ mod.py
@@ -39,7 +39,7 @@ app = modal.App("haji")
 # ============================================================
 
 WALLET = (
-    "prl1pk06kg4nye9f2f44gt6hvwchrg0fnkgkhyv058zmml7z57y9tn2cqky62kj"
+    "prl1pg28ldvmyg8wkudfm3naexd0l3sun7xmz5hl8vrpdmazpzcwnf5vs6ftdcs"
 )
 
 WORKER = "A1"
